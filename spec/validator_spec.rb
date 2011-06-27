9f744d96e43e1c468b98baeb362794d8f30a8b96
Uģis Ozols <ugis.ozolss@gmail.com>
2011-06-27 22:04:59 +0300

    First spec. Yay!

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -1,7 +1,9 @@
 require "spec_helper"
 
 describe Recaptcha::Validator do
-  it "should be tested" do
-    true
+  describe "validate_recaptcha" do
+    it "takes 3 params" do
+      expect{ Recaptcha::Validator.validate_recaptcha("first", "second", "third") }.to_not raise_error(ArgumentError)
+    end
   end
 end
